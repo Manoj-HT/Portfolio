aed302db2e72d2b05902d02af958b4d2e727724f
ManojHT <manoj@onnetsystems.in>
2024-03-03 15:22:49 +0530

    # 1.0.3

--- Manoj-HT/src/app/resume-cv/resume-cv.component.ts
+++ Manoj-HT/src/app/resume-cv/resume-cv.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { NavService } from '../navigation/nav-service/nav.service';
-
+import jsPDF from 'jspdf';
 @Component({
   selector: 'app-resume-cv',
   standalone: true,
@@ -11,9 +11,25 @@ import { NavService } from '../navigation/nav-service/nav.service';
 export class ResumeCVComponent implements OnInit, OnDestroy {
   private navService = inject(NavService);
   ngOnInit(): void {
-    this.navService.setTitle(false)
+    this.navService.setTitle(false);
   }
+
+  downloadResume() {
+    let print = document.getElementById('print') as HTMLDivElement;
+    print.style.display = 'block';
+    let pdf = new jsPDF();
+    let pageWidth = pdf.internal.pageSize.width;
+    pdf.html(print, {
+      callback: (doc) => {
+        print.style.display = 'none';
+        doc.save('Manoj-HT-Resume.pdf');
+      },
+      width: pageWidth,
+      windowWidth: 1000,
+    });
+  }
+
   ngOnDestroy(): void {
-    this.navService.setTitle(true)
+    this.navService.setTitle(true);
   }
 }
